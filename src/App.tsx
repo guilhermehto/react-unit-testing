19e3bdfccafd23c781281d5efe1e9cea27361043
Guilherme Oliveira <guilherme.oliveira@eroad.com>
2022-05-02 16:45:45 +1200

    feat: add button to main card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { Button } from './components/Button'
 import { Card } from './components/Card'
 
 function App() {
 	return (
-		<div className="flex items-center justify-center h-screen w-screen">
-			<Card isLoading={false} title="Title" footer={'footer'}>
+		<div className="flex flex-col items-center justify-center h-screen w-screen">
+			<Card isLoading={false} title="Title" footer={<Button>click me</Button>}>
 				This is content :)
 			</Card>
 		</div>
