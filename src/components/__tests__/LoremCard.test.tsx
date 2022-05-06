2f25186bc8fbb9328bb2606aa5c58d86636484d9
Guilherme Oliveira <guilherme.oliveira@eroad.com>
2022-05-06 16:15:15 +1200

    chore: clean up tests

--- src/components/__tests__/LoremCard.test.tsx
+++ src/components/__tests__/LoremCard.test.tsx
@@ -1,29 +1,18 @@
 import React from 'react'
-import { act } from 'react-dom/test-utils'
 
 import { mount } from 'enzyme'
 
 import { LoremCard } from '../LoremCard'
 
-jest.mock('../../clients/lorem-client', () => ({
-	loremClient: {
-		getLorem: () => 'lorem phrase',
-	},
-}))
-
 describe('LoremCard tests', () => {
 	it('sets loading when button clicked', () => {
 		let wrapper: any
 
-		act(() => {
-			wrapper = mount(<LoremCard />)
-		})
+		wrapper = mount(<LoremCard />)
 
 		const button = wrapper.find('Button')
 
-		act(() => {
-			button.simulate('click')
-		})
+		button.simulate('click')
 
 		expect(wrapper.find('Card').prop('isLoading')).toBe(true)
 	})
@@ -32,15 +21,11 @@ describe('LoremCard tests', () => {
 		let wrapper: any
 		const fetchPhraseMock = jest.fn()
 
-		act(() => {
-			wrapper = mount(<LoremCard fetchPhrase={fetchPhraseMock} />)
-		})
+		wrapper = mount(<LoremCard fetchPhrase={fetchPhraseMock} />)
 
 		const button = wrapper.find('Button')
 
-		act(() => {
-			button.simulate('click')
-		})
+		button.simulate('click')
 
 		expect(fetchPhraseMock).toHaveBeenCalled()
 	})
